ae8c4531c582ce0f4659241152fb8fac792b1e82
4geniu <taketubo0714@gmail.com>
2023-11-15 23:21:30 +0900

    debug: テストケースの修正

--- __test__/posRotation.test.js
+++ __test__/posRotation.test.js
@@ -19,10 +19,10 @@ test("(0, 1, 0)を(0, 0, 90)回転させると(-1, 0, 0)になるか調べる。
   expect(rotatedPos.x).toEqual(-1);
   
   //yの値を調べる
-  expect(rotatedPos.y).toEqual
+  expect(rotatedPos.y).toEqual(0);
 
   //zの値を調べる
-  expect(rotatedPos.z).toEqual
+  expect(rotatedPos.z).toEqual(0);
 });
 test("(0, 0, 1)を(90, 0, 0)回転させると(0, -1, 0)になるか調べる。", ()=>{
   let rotatedPos = Math.rotatePos(0, 0, 1, 90, 0, 0);
