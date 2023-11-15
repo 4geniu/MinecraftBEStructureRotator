4c45e242f32cd21d0df42b8223c16db7a61a6921
4geniu <taketubo0714@gmail.com>
2023-11-15 23:43:24 +0900

    Mathematicsに追加した関数のテストケースの追加

---
+++ __test__/calculationCenterPos.test.js
@@ -0,0 +1,19 @@
+const Math = require('../scripts/Mathematics');
+
+test("(0,0,0)と(100,100,100)との中点を求める。", ()=>{
+  let startPos = {x: 0, y: 0, z: 0};
+  let endPos = {x: 100, y: 100, z: 100};
+  let centerPos = Math.calcCenterPos(startPos, endPos);
+  expect(centerPos.x).toEqual(50);
+  expect(centerPos.y).toEqual(50);
+  expect(centerPos.z).toEqual(50);
+});
+
+test("(12, 82, 832)と(812, 8, 124)との中点を求める。", ()=>{
+  let startPos = {x: 12, y: 82, z: 832};
+  let endPos = {x: 812, y: 8, z: 124};
+  let centerPos = Math.calcCenterPos(startPos, endPos);
+  expect(centerPos.x).toEqual(412);
+  expect(centerPos.y).toEqual(45);
+  expect(centerPos.z).toEqual(478);
+});
---
+++ __test__/calculationDistance.test.js
@@ -0,0 +1,10 @@
+const Math = require('../scripts/Mathematics');
+
+test("Center(50, 50, 50)とPos(100, 100, 100)との距離を求める。", ()=>{
+  let centerPos = {x: 50, y: 50, z: 50};
+  let pos = {x: 100, y: 100, z: 100};
+  let distance = Math.calcDistanceFromCenter(centerPos, pos);
+  expect(distance.x).toEqual(50);
+  expect(distance.y).toEqual(50);
+  expect(distance.z).toEqual(50);
+});
