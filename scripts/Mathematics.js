6ba18f59cef08e1c529d05cbb60a89e70d1060a9
4geniu <taketubo0714@gmail.com>
2023-11-15 23:26:01 +0900

    debug: Mathematics.rotatePos()の修正

--- scripts/Mathematics.js
+++ scripts/Mathematics.js
@@ -7,10 +7,10 @@ var Mathematics = {
       let yRad = yDeg * Math.PI / 180;
       let zRad = zDeg * Math.PI / 180;
 
-      let rotatedX = xPos * (Math.cos(yRad) * Math.cos(zRad)) + yPos * (Math.cos(yRad) * Math.sin(zRad)) - zPos * Math.sin(yRad);
-      let rotatedY = xPos * (Math.cos(xRad) * Math.sin(zRad) + Math.cos(zRad) * Math.sin(xRad) * Math.sin(yRad)) + yPos * (Math.cos(xRad) * Math.cos(zRad) - Math.sin(xRad) * Math.sin(yRad) * Math.sin(zRad)) + zPos * Math.cos(yRad) * Math.sin(xRad);
+      let rotatedX = xPos * Math.cos(yRad) * Math.cos(zRad) - yPos * Math.cos(yRad) * Math.sin(zRad) + zPos * Math.sin(yRad);
+      let rotatedY = xPos * (Math.cos(xRad) * Math.sin(zRad) + Math.cos(zRad) * Math.sin(xRad) * Math.sin(yRad)) + yPos * (Math.cos(xRad) * Math.cos(zRad) - Math.sin(xRad) * Math.sin(yRad) * Math.sin(zRad)) - zPos * Math.cos(yRad) * Math.sin(xRad);
       let rotatedZ = xPos * (Math.sin(xRad) * Math.sin(zRad) - Math.cos(xRad) * Math.cos(zRad) * Math.sin(yRad)) + yPos * (Math.cos(zRad) * Math.sin(xRad) + Math.cos(xRad) * Math.sin(yRad) * Math.sin(zRad)) + zPos * Math.cos(xRad) * Math.cos(yRad);
-
+      
       return {
         x: Math.round(rotatedX),
         y: Math.round(rotatedY),
