e77bbf646c2ac76a9dc0b96b627d3344131421a7
4geniu <taketubo0714@gmail.com>
2023-11-15 23:18:54 +0900

    debug: Modification of test cases

--- __test__/posRotation.test.js
+++ __test__/posRotation.test.js
@@ -3,44 +3,35 @@ const Math = require('../scripts/Mathematics');
 test("(1, 0, 0)を(0, 90, 0)回転させると(0, 0, -1)になるか調べる。", ()=>{
   let rotatedPos = Math.rotatePos(1, 0, 0, 0, 90, 0);
   //xの値を調べる
-  expect(rotatedPos.x).toBeGreaterThanOrEqual(-0.001);
-  expect(rotatedPos.x).toBeLessThanOrEqual(0.001);
+  expect(rotatedPos.x).toEqual(0);
   
   //yの値を調べる
-  expect(rotatedPos.y).toBeGreaterThanOrEqual(-0.001);
-  expect(rotatedPos.y).toBeLessThanOrEqual(0.001);
+  expect(rotatedPos.y).toEqual(0);
 
   //zの値を調べる
-  expect(rotatedPos.z).toBeGreaterThanOrEqual(-1.001);
-  expect(rotatedPos.z).toBeLessThanOrEqual(-0.999);
+  expect(rotatedPos.z).toEqual(-1);
 });
 
 
 test("(0, 1, 0)を(0, 0, 90)回転させると(-1, 0, 0)になるか調べる。", ()=>{
   let rotatedPos = Math.rotatePos(0, 1, 0, 0, 0, 90);
   //xの値を調べる
-  expect(rotatedPos.x).toBeGreaterThanOrEqual(-1.001);
-  expect(rotatedPos.x).toBeLessThanOrEqual(-0.999);
+  expect(rotatedPos.x).toEqual(-1);
   
   //yの値を調べる
-  expect(rotatedPos.y).toBeGreaterThanOrEqual(-0.001);
-  expect(rotatedPos.y).toBeLessThanOrEqual(0.001);
+  expect(rotatedPos.y).toEqual
 
   //zの値を調べる
-  expect(rotatedPos.z).toBeGreaterThanOrEqual(-0.001);
-  expect(rotatedPos.z).toBeLessThanOrEqual(0.001);
+  expect(rotatedPos.z).toEqual
 });
 test("(0, 0, 1)を(90, 0, 0)回転させると(0, -1, 0)になるか調べる。", ()=>{
   let rotatedPos = Math.rotatePos(0, 0, 1, 90, 0, 0);
   //xの値を調べる
-  expect(rotatedPos.x).toBeGreaterThanOrEqual(-0.001);
-  expect(rotatedPos.x).toBeLessThanOrEqual(0.001);
+  expect(rotatedPos.x).toEqual(0);
   
   //yの値を調べる
-  expect(rotatedPos.y).toBeGreaterThanOrEqual(-1.001);
-  expect(rotatedPos.y).toBeLessThanOrEqual(-0.999);
+  expect(rotatedPos.y).toEqual(-1);
 
   //zの値を調べる
-  expect(rotatedPos.z).toBeGreaterThanOrEqual(-0.001);
-  expect(rotatedPos.z).toBeLessThanOrEqual(0.001);
+  expect(rotatedPos.z).toEqual(0);
 });
